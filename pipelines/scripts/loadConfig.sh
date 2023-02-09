ca3202f9f53a7eb7018e72842f8cc12ee3bffb7d
chini007 <73479119+chini007@users.noreply.github.com>
2023-02-09 21:32:25 +0100

    Update Load Config

--- pipelines/scripts/loadConfig.sh
+++ pipelines/scripts/loadConfig.sh
@@ -7,6 +7,5 @@
     echo "##vso[task.setvariable variable=exporter_user]`yq -e ".tenant.exporter_username" configs/dev.yml`"
     echo "##vso[task.setvariable variable=source_type]`yq -e ".tenant.type" configs/dev.yml`"
     PROJECTNAME=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$PROJECTNAME")
-    echo ${PROJECTNAME}
     echo "##vso[build.updatebuildnumber]$(Build.BuildNumber)-${PROJECTNAME}"
-    echo $(System.DefaultWorkingDirectory)
+    
