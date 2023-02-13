a43ff5a71ebb98953b83d27bd71c5aa0dddc060c
chini007 <73479119+chini007@users.noreply.github.com>
2023-02-13 22:35:56 +0100

    update scripts

--- pipelines/scripts/createProject.sh
+++ pipelines/scripts/createProject.sh
@@ -50,8 +50,7 @@ if [ -z "$name" ];   then
     --header 'Accept: application/json' \
     --data-raw "$json" -u ${admin_user}:${admin_password})
 
-    namecreated=$(jq '.output.name // empty' <<< "$projectName" )
-    #namecreated=echo "$projectName" | jq -r '.output.name // empty'
+    namecreated=$(echo "$projectName" | jq  '.output.name // empty')
     echo ${namecreated}
 
     if [ ! -z "$namecreated" ]; then
