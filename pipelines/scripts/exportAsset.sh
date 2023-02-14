9f4428e006096604f477128e2a93498f0e925ec2
chini007 <73479119+chini007@users.noreply.github.com>
2023-02-14 17:46:55 +0100

    url encoded

--- pipelines/scripts/exportAsset.sh
+++ pipelines/scripts/exportAsset.sh
@@ -85,8 +85,8 @@ if [ "${assetType}" = "workflow" ]; then
         echo "Download link retreival Failed:" ${linkJson}
         exit 1
     fi
-    
-  curl --location --request GET ${downloadURL} 
+    downloadURL=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$(downloadURL)")
+    curl --location --request GET ${downloadURL} --output ${assetID}.zip
 
     FILE=./${assetID}.zip
     if [ -f "$FILE" ]; then
