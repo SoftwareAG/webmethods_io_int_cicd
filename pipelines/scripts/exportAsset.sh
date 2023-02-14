266f8e349d67ec6fd4285e3d55d54e14efb5d892
chini007 <73479119+chini007@users.noreply.github.com>
2023-02-14 16:46:35 +0100

    export fixed

--- pipelines/scripts/exportAsset.sh
+++ pipelines/scripts/exportAsset.sh
@@ -87,7 +87,7 @@ if [ "${assetType}" = "workflow" ]; then
         exit 1
     fi
     
-  downloadJsron=$(curl --location --request --data-urlencode GET "${downloadURL}" --output ${assetID}.zip)
+  downloadJsron=$(curl --location --request GET "${downloadURL}" --output ${assetID}.zip)
 
     FILE=./${assetID}.zip
     if [ -f "$FILE" ]; then
