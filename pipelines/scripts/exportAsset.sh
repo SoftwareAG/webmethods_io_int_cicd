e1f320af87cb765d591e71db7521750d83901b8b
chini007 <73479119+chini007@users.noreply.github.com>
2023-03-06 18:10:27 +0100

    RD Update

--- pipelines/scripts/exportAsset.sh
+++ pipelines/scripts/exportAsset.sh
@@ -209,17 +209,13 @@ echod "curl --location --request GET ${PROJECT_ID_URL}  \
 --header 'Accept: application/json' \
 -u ${admin_user}:${admin_password}"
 
-curl --location --request GET ${PROJECT_ID_URL}  \
---header 'Content-Type: application/json' \
---header 'Accept: application/json' \
--u ${admin_user}:${admin_password}
-
 projectIDJson=$(curl --location --request GET ${PROJECT_ID_URL}  \
 --header 'Content-Type: application/json' \
 --header 'Accept: application/json' \
 -u ${admin_user}:${admin_password})
 
-echod "projectIDJson:" ${projectIDJson}
+
+echod "projectIDJson:" $projectIDJson
 
 projectID=$(echo "$projectIDJson" | jq '.output.uid // empty')
 
